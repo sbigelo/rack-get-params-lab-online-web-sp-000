a77197f83e1dc88b99a7ba3e2e2f158c57939c6f
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 22:08:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,12 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else 
+        @@cart.each {|key| resp.write "#{key}"}
+        end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
